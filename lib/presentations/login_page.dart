acf041aa25691bd03c4e87cb71898c9e41db3abe
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-12-13 11:05:58 +0900

    add login page with riverpod

--- lib/presentations/login_page.dart
+++ lib/presentations/login_page.dart
@@ -6,15 +6,28 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (context, ref, child) {
-      final userState = ref.watch(loginUserState);
-      final userNotifier = ref.read(loginUserState.notifier);
+      final userState = ref.watch(loginUserState); // 생태 변경 감지용
+      final userNotifier = ref.read(loginUserState.notifier); // 함수 실행용
 
       return Scaffold(
         appBar: AppBar(title: Text('로그인')),
         body: Center(
           child: userState.when(
-            data: (user) => Text('로그인됨: ${user.name}'),
-            loading: () {
+            data: (user){ // 로그인이 되어있는 경우
+              return Column(
+                children: [
+                  Text('로그인됨: ${user.name}'),
+                  ElevatedButton(
+                  onPressed: () async {
+                    // 로그인 로직 구현
+                    await userNotifier.login("", "");
+                  },
+                  child: Text('홈으로 이동 '),
+                ),
+                ],
+              );
+            },
+            loading: () { // 로그인이 되어있지 않은 경우
               return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -26,7 +39,7 @@ class LoginPage extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () async {
                     // 로그인 로직 구현
-                    await userNotifier.login("", "");
+                    await userNotifier.login("id", "pw");
                   },
                   child: Text('로그인'),
                 ),
--- lib/states/loing_user_state.dart
+++ lib/states/loing_user_state.dart
@@ -2,28 +2,15 @@ import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '/models/user.dart';
 
-enum LoginState {
-  none,
-  email,
-  naver,
-  facebook,
-  apple
-}
-
-
 class AsyncLoginUserNotifier extends StateNotifier<AsyncValue<User>> {
-  AsyncLoginUserNotifier() : super(AsyncValue.loading());
+  AsyncLoginUserNotifier() : super(AsyncValue.loading()); // 기본값을 로딩 상태로 설정
 
-  loading() {
-    return AsyncValue.data(User(id: -1, name: 'John Doe', email: ''));
-  }
-  
   Future<void> login(String username, String password) async {
     state = AsyncValue.loading();
     try {
       // 여기서 실제 로그인 로직을 구현합니다.
       // 예를 들어, 서버 API 호출을 통해 사용자 정보를 가져옵니다.
-      await Future.delayed(Duration(seconds: 2)); // 로그인 시뮬레이션
+      await Future.delayed(Duration(seconds: 1)); // 로그인 시뮬레이션
 
       // 로그인 성공 시
       final user = User(id: 1, name: 'John Doe', email: 'john@example.com');
